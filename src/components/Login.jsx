8b3721943c3b2d3a27f8a250da96f3e349ed8aae
Sarthak Garg <sarthakgarg23650@gmail.com>
2025-01-11 11:36:11 +0530

    connections and reuests received page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-    const [emailId, setEmailId] = useState('elon@gmail.com');
-    const [password, setPassword] = useState('Elon@12345');
+    const [emailId, setEmailId] = useState('virat@gmail.com');
+    const [password, setPassword] = useState('Virat@12345');
     const [error, setError] = useState('');
     const dispatch = useDispatch();
     const navigate = useNavigate();
