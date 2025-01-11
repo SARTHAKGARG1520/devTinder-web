3bb237f651c103cbdb8d499adc541072a6c368d8
Sarthak Garg <sarthakgarg23650@gmail.com>
2025-01-11 14:49:39 +0530

    bug fixes and signup functionality built

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,11 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-    const [emailId, setEmailId] = useState('elon@gmail.com');
-    const [password, setPassword] = useState('Elon@12345');
+    const [emailId, setEmailId] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [isLogin, setIsLogin] = useState(false);
     const [error, setError] = useState('');
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -28,11 +31,36 @@ const Login = () => {
             setError(err?.response?.data || "Something went wrong");
         }
     }
+    const onSignUpHandle = async () => {
+        try {
+            const res = await axios.post(BASE_URL + '/signup', {
+                firstName, lastName, emailId, password
+            }, { withCredentials: true });
+            dispatch(addUser(res.data));
+            return navigate('/profile');
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
     return (
         <div className="flex justify-center m-6">
             <div className="card bg-base-300 w-96 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title justify-center">Login</h2>
+                    <h2 className="card-title justify-center">{isLogin ? 'Login' : 'Sign Up'}</h2>
+                    {!isLogin && <><label className="form-control w-full max-w-xs my-1">
+                        <div className="label">
+                            <span className="label-text">First Name</span>
+                        </div>
+                        <input type="text" value={firstName} className="input input-bordered w-full max-w-xs"
+                            onChange={(e) => setFirstName(e.target.value)} />
+                    </label>                    <label className="form-control w-full max-w-xs my-1">
+                            <div className="label">
+                                <span className="label-text">Last Name</span>
+                            </div>
+                            <input type="text" value={lastName} className="input input-bordered w-full max-w-xs"
+                                onChange={(e) => setLastName(e.target.value)} />
+                        </label></>}
                     <label className="form-control w-full max-w-xs my-1">
                         <div className="label">
                             <span className="label-text">Email ID</span>
@@ -44,13 +72,21 @@ const Login = () => {
                         <div className="label">
                             <span className="label-text">Password</span>
                         </div>
-                        <input type="password" value={password} className="input input-bordered w-full max-w-xs"
+                        <input type="pasword" value={password} className="input input-bordered w-full max-w-xs"
                             onChange={(e) => setPassword(e.target.value)} />
                     </label>
                     <div className="card-actions justify-center">
                         <p className="text-red-500">{error}</p>
-                        <button className="btn btn-primary" onClick={onLoginHandle}>Login</button>
+                        <button className="btn btn-primary" onClick={isLogin ? onLoginHandle : onSignUpHandle}>{isLogin ? 'Login' : 'Sign Up'} </button>
                     </div>
+                    <p
+                        className="m-auto cursor-pointer py-2"
+                        onClick={() => setIsLogin((value) => !value)}
+                    >
+                        {isLogin
+                            ? "New User? Signup Here"
+                            : "Existing User? Login Here"}
+                    </p>
                 </div>
             </div>
         </div>);
