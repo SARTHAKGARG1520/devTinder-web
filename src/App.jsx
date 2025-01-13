4ad9a8eace37cdf9956950bc2181f525b0bc9b7a
Sarthak Garg <sarthakgarg23650@gmail.com>
2025-01-13 19:17:51 +0530

    razorpay integration done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import appStore from './utils/appStore';
 import Feed from './components/Feed';
 import Connections from './components/Connections';
 import Requests from './components/Requests';
+import Premium from './components/Premium';
 function App() {
   return (
     <>
@@ -17,8 +18,9 @@ function App() {
               <Route path='/' element={<Feed />} />
               <Route path='/login' element={<Login />} />
               <Route path='/profile' element={<Profile />} />
-              <Route path='/connections' element={<Connections />}/>
-              <Route path='/requests' element={<Requests />}/>
+              <Route path='/connections' element={<Connections />} />
+              <Route path='/requests' element={<Requests />} />
+              <Route path='/premium' element={<Premium />} />
             </Route>
           </Routes>
         </BrowserRouter>
